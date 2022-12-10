edf725985f33013b867e520dec0ed78075abef12
tomasmatus <tomatus777@gmail.com>
2022-12-10 13:18:05 +0100

    Add docstrings

--- part2/analysis.py
+++ part2/analysis.py
@@ -12,6 +12,12 @@ import zipfile
 
 # Ukol 1: nacteni dat ze ZIP souboru
 def load_data(filename : str) -> pd.DataFrame:
+    """Return datataframe from nested zipfile
+
+    Keyword arguments:
+    filename -- path to zipfile of zipfiles containing data in csv
+    """
+
     # tyto konstanty nemente, pomuzou vam pri nacitani
     headers = ["p1", "p36", "p37", "p2a", "weekday(p2a)", "p2b", "p6", "p7", "p8", "p9", "p10", "p11", "p12", "p13a",
                 "p13b", "p13c", "p14", "p15", "p16", "p17", "p18", "p19", "p20", "p21", "p22", "p23", "p24", "p27", "p28",
@@ -50,16 +56,26 @@ def load_data(filename : str) -> pd.DataFrame:
 
 # Ukol 2: zpracovani dat
 def parse_data(df : pd.DataFrame, verbose : bool = False) -> pd.DataFrame:
+    """Return parsed dataframe for further use
+
+    Keyword arguments:
+    df -- unparsed pandas dataframe
+    verbose -- when set to True print memory usage of original and parsed dataframe (default: False)
+    """
+
     df2 = df.copy()
 
     df2 = df2.drop_duplicates(subset="p1", keep="first")
+    # convert all but selected columns to category
     to_convert = [x for x in df2 if x not in ["p1", "p2a", "p13a", "p13b", "p13c", "p14", "p34", "p37", "p53", "region", "d", "e"]]
     df2[to_convert] = df2[to_convert].astype("category")
+
     for col in ["d", "e"]:
         df2[col] = pd.to_numeric(df2[col], errors="coerce")
 
     df2["date"] = pd.to_datetime(df2["p2a"]).astype('datetime64[ns]')
 
+    # memory usage statistics
     if verbose:
         print(f'orig_size={df.memory_usage(deep=True).sum() / 1_000_000} MB')
         print(f'new_size={df2.memory_usage(deep=True).sum() / 1_000_000} MB')
@@ -70,6 +86,13 @@ def parse_data(df : pd.DataFrame, verbose : bool = False) -> pd.DataFrame:
 # Ukol 3: počty nehod v jednotlivých regionech podle viditelnosti
 def plot_visibility(df: pd.DataFrame, fig_location: str = None,
                     show_figure: bool = False):
+    """Generate graph showing connection between visibility and number of crashes
+
+    Keyword arguments:
+    df -- parsed pandas dataframe
+    fig_location -- path where to save generated graph (default: None)
+    show_figure -- show graph when it is generated (default: False)
+    """
 
     def replace_conditions(row):
         if row["p19"] == 1:
@@ -120,6 +143,13 @@ def plot_visibility(df: pd.DataFrame, fig_location: str = None,
 # Ukol4: druh srážky jedoucích vozidel
 def plot_direction(df: pd.DataFrame, fig_location: str = None,
                    show_figure: bool = False):
+    """Generate graph showing number of crashes per direction
+
+    Keyword arguments:
+    df -- parsed pandas dataframe
+    fig_location -- path where to save generated graph (default: None)
+    show_figure -- show graph when it is generated (default: False)
+    """
 
     def replace_crash_type(row):
         if row["p7"] == 1:
@@ -135,7 +165,8 @@ def plot_direction(df: pd.DataFrame, fig_location: str = None,
     df2["p7"] = df2.apply(lambda row: replace_crash_type(row), axis=1)
     df2["sum_helper"] = 1
 
-    data = df2.groupby(["region", df2["date"].dt.month, "p7"]).agg({"sum_helper": "sum"}).reset_index()
+    data = df2.groupby(["region", df2["date"].dt.month, "p7"]
+                       ).agg({"sum_helper": "sum"}).reset_index()
 
     sns.set_style("darkgrid")
     g = sns.catplot(data=data, x="date", y="sum_helper",
@@ -149,7 +180,7 @@ def plot_direction(df: pd.DataFrame, fig_location: str = None,
     g.set_titles("Kraj: {col_name}")
     g.set_xlabels("Měsíc")
     g.set_ylabels("Počet nehod")
-    g.legend.set(title="Druh srážky", alignment="left")
+    g.legend.set(title="Druh srážky")
     g.tight_layout()
 
     if show_figure:
@@ -162,6 +193,13 @@ def plot_direction(df: pd.DataFrame, fig_location: str = None,
 # Ukol 5: Následky v čase
 def plot_consequences(df: pd.DataFrame, fig_location: str = None,
                     show_figure: bool = False):
+    """Generate graph showing severity of injuries
+
+    Keyword arguments:
+    df -- parsed pandas dataframe
+    fig_location -- path where to save generated graph (default: None)
+    show_figure -- show graph when it is generated (default: False)
+    """
 
     def injury_to_text(row):
         if row["p13a"] > 0:
@@ -178,10 +216,14 @@ def plot_consequences(df: pd.DataFrame, fig_location: str = None,
     df2 = df2[df2["region"].isin(regions)]
     df2["injury"] = df2.apply(lambda row: injury_to_text(row), axis=1)
 
-    table = pd.pivot_table(df2, index=["date"], values="p9", columns=["injury", "region"], aggfunc="count")
+    table = pd.pivot_table(df2, index=["date"], values="p9",
+                           columns=["injury", "region"], aggfunc="count")
     table = table.drop(columns=["Žádné"])
+    # resample by date, sum all values in a month
     table = table.resample('M').sum().stack().reset_index()
-    table = table.melt(["date", "region"], ["Lehké zranění", "Těžké zranění", "Usmrcení"], value_name="zraneni")
+    table = table.melt(["date", "region"],
+                       ["Lehké zranění", "Těžké zranění", "Usmrcení"],
+                       value_name="zraneni")
 
     g = sns.relplot(data=table, x="date", y="zraneni",
                     col="region", col_wrap=2, hue="injury",
@@ -190,7 +232,9 @@ def plot_consequences(df: pd.DataFrame, fig_location: str = None,
                     facet_kws={"sharex": False})
 
     g.set_xlabels("")
+    # set date format on xticks so it's compatible with desired result
     g.set(xticks=[f'20{year}-01' for year in range(16,23)], xmargin=0)
+    # set ticks to monty/year format
     g.set_xticklabels([f'01/{year}' for year in range(16,23)])
     g.set_ylabels("Počet nehod")
     g.set_titles("Kraj: {col_name}")
@@ -211,9 +255,9 @@ if __name__ == "__main__":
     df = load_data("data/data.zip")
     df2 = parse_data(df, True)
 
-    plot_visibility(df2, "01_visibility.png")
-    plot_direction(df2, "02_direction.png")
-    plot_consequences(df2, "03_consequences.png")
+    plot_visibility(df2, "01_visibility.png", True)
+    plot_direction(df2, "02_direction.png", True)
+    plot_consequences(df2, "03_consequences.png", True)
 
 
 # Poznamka:
