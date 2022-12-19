dda0ca2b0213494e5ca0ca310fd66e5a73e1c88d
tomasmatus <tomatus777@gmail.com>
2022-12-19 18:34:35 +0100

    plot_cluster

--- part3/geo.py
+++ part3/geo.py
@@ -7,6 +7,7 @@ import contextily as ctx
 import sklearn.cluster
 import numpy as np
 # muzete pridat vlastni knihovny
+from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
 
 
 def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
@@ -17,7 +18,6 @@ def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
     df2 = df2[df2["d"].notna() & df2["e"].notna()]
 
     df2["date"] = pd.to_datetime(df2["p2a"]).astype('datetime64[ns]')
-    df2["p11"] = pd.to_numeric(df2["p11"], errors="coerce")
 
     # convert all but selected columns to category
     #to_convert = [x for x in df if x not in ["p1", "p2a", "p13a", "p13b", "p13c", "p14", "p34", "p37", "p53", "region", "d", "e"]]
@@ -58,7 +58,38 @@ def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
 def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
                  show_figure: bool = False):
     """ Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru """
-    pass
+    region = "ZLK"
+    geo = gdf[gdf["region"] == region].copy().to_crs("EPSG:3857")
+
+    # select roads of 1st, 2nd and 3rd class
+    geo = geo[(geo["p36"] >= 1) & (geo["p36"] <= 3)]
+
+    # matrix with x, y coordinates
+    coords = np.dstack([geo.geometry.x, geo.geometry.y]).reshape(-1, 2)
+
+    # I picked Agglomerative Clustering and 25 clusters because the result
+    # looks the most self describing
+    geo["cluster"] = sklearn.cluster.AgglomerativeClustering(n_clusters=25).fit(coords).labels_
+    geo = geo.dissolve(by="cluster", aggfunc={"p1": "count"})
+
+    fig, ax = plt.subplots(1, 1, figsize=(10, 8))
+
+    # make legend same width as map
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("bottom", size="5%", pad=0.25)
+
+    geo.plot(ax=ax, markersize=1, column="p1", legend=True,
+             legend_kwds={"orientation": "horizontal", "cax": cax})
+    ax.set_axis_off()
+    ax.set_title(f"Nehody v {region} kraji na silnicích 1., 2. a 3. třídy")
+    ctx.add_basemap(ax, crs=geo.crs.to_string(), source=ctx.providers.Stamen.TonerLite, attribution_size=6)
+    plt.tight_layout()
+
+    if show_figure:
+        plt.show()
+
+    if fig_location:
+        fig.savefig(fig_location, bbox_inches="tight")
 
 
 if __name__ == "__main__":
